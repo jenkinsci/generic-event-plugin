26cdb9325b035b42a8f21ebba7149cfc15b1ff6c
ng.kasatkin <nikita.kasatkin44@gmail.com>
2022-07-20 23:55:49 +0300

    1.0.2

--- src/main/java/io/jenkins/plugins/generic/event/HttpEventSender.java
+++ src/main/java/io/jenkins/plugins/generic/event/HttpEventSender.java
@@ -1,37 +1,20 @@
 package io.jenkins.plugins.generic.event;
 
-import java.net.URI;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.entity.EntityBuilder;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
 import org.apache.hc.core5.concurrent.FutureCallback;
 import org.apache.hc.core5.http.*;
-import org.apache.hc.core5.http.impl.bootstrap.HttpAsyncRequester;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-
 import io.jenkins.plugins.generic.event.json.EventJsonConfig;
 import io.jenkins.plugins.generic.event.transformer.EventDataTransformers;
 import net.sf.json.JSONObject;
-import org.apache.hc.core5.http.nio.AsyncClientEndpoint;
-import org.apache.hc.core5.http.nio.entity.StringAsyncEntityConsumer;
-import org.apache.hc.core5.http.nio.support.AsyncRequestBuilder;
-import org.apache.hc.core5.http.nio.support.BasicResponseConsumer;
-import org.apache.hc.core5.http2.config.H2Config;
-import org.apache.hc.core5.http2.frame.RawFrame;
-import org.apache.hc.core5.http2.impl.nio.H2StreamListener;
-import org.apache.hc.core5.http2.impl.nio.bootstrap.H2RequesterBootstrap;
-import org.apache.hc.core5.reactor.IOReactorConfig;
-import org.apache.hc.core5.util.Timeout;
+import org.apache.hc.core5.util.TimeValue;
 
 /**
  * An implementation of EventSender, which sends event body via HTTP.
@@ -41,140 +24,64 @@ import org.apache.hc.core5.util.Timeout;
 public class HttpEventSender implements EventSender {
 
     private final Logger logger = Logger.getLogger(HttpEventSender.class.getName());
-
-    final IOReactorConfig ioReactorConfig = IOReactorConfig.custom()
-            .setSoTimeout(5, TimeUnit.SECONDS)
-            .build();
-
-    final H2Config h2Config = H2Config.custom()
-            .setPushEnabled(false)
-            .setMaxConcurrentStreams(100)
-            .build();
-
-    final HttpAsyncRequester requester = H2RequesterBootstrap.bootstrap()
-            .setIOReactorConfig(ioReactorConfig)
-            .setH2Config(h2Config)
-            .setStreamListener(new H2StreamListener() {
-                @Override
-                public void onHeaderInput(HttpConnection httpConnection, int i, List<? extends Header> list) {
-
-                }
-
-                @Override
-                public void onHeaderOutput(HttpConnection httpConnection, int i, List<? extends Header> list) {
-
-                }
-
-                @Override
-                public void onFrameInput(HttpConnection httpConnection, int i, RawFrame rawFrame) {
-
-                }
-
-                @Override
-                public void onFrameOutput(HttpConnection httpConnection, int i, RawFrame rawFrame) {
-
-                }
-
-                @Override
-                public void onInputFlowControl(HttpConnection httpConnection, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void onOutputFlowControl(HttpConnection httpConnection, int i, int i1, int i2) {
-
-                }
-            })
-            .create();
+    CustomCloseableHttpAsyncClient myClient = new CustomCloseableHttpAsyncClient();
 
     final String receiver = EventGlobalConfiguration.get().getReceiver();
-    final String uri = "http://localhost:8000/event-get";
 
+    @Override
     public void send(Event event) {
         EventDataTransformers.INSTANCE.transform(event);
 
         try {
-            requester.start();
-
-            final HttpHost target = HttpHost.create(URI.create(receiver));
-            final Future<AsyncClientEndpoint> future = requester.connect(target, Timeout.ofSeconds(60));
-            final AsyncClientEndpoint clientEndpoint = future.get();
-            final CountDownLatch latch = new CountDownLatch(1);
-
-            clientEndpoint.execute(
-                    AsyncRequestBuilder.get()
-                            .setUri(uri)
-                            .build(),
-                    new BasicResponseConsumer<>(new StringAsyncEntityConsumer()),
-                    new FutureCallback<Message<HttpResponse, String>>() {
-                        @Override
-                        public void completed(Message<HttpResponse, String> message) {
-                            latch.countDown();
-                            final HttpResponse response = message.getHead();
-                            final String body = message.getBody();
-
-                            System.out.println("/event-get response code: " + response.getCode());
-                            System.out.println(body);
-                        }
-
-                        @Override
-                        public void failed(Exception e) {
-                            latch.countDown();
-                            System.out.println(uri + "->" + e);
-                        }
-
-                        @Override
-                        public void cancelled() {
-                            latch.countDown();
-                            System.out.println(uri + " cancelled");
-                        }
-                    }
-            );
-
-            latch.await();
-//            clientEndpoint.releaseAndDiscard();  // Нужно завершать только при завершении процесса Jenkins
-//            System.out.println("Shutting down I/O reactor");
-//            requester.initiateShutdown();
-
-        } catch (Exception e) {
-            logger.log(Level.WARNING, "Failed to send event to " + receiver, e);
-        }
-
-
-
-
-
-    }
-
-//    @Override
-    public void sendSync(Event event) {
-        EventDataTransformers.INSTANCE.transform(event);
-
-        // serialize event to JSON string
-        String receiver = EventGlobalConfiguration.get().getReceiver();
-        if (StringUtils.isBlank(receiver)) {
-            logger.info("Skipped event sending due to receiver URL not set");
-            return;
-        }
-        // TODO Make HTTP request asynchronous
-        try (final CloseableHttpClient client = HttpClients.createDefault()) {
-            String eventJSON = JSONObject.fromObject(event, new EventJsonConfig()).toString(4);
-            HttpEntity httpEntity = EntityBuilder.create()
-                    .setContentType(ContentType.APPLICATION_JSON)
-                    .setText(eventJSON)
-                    .build();
-            HttpPost httpPost = new HttpPost(receiver);
-            httpPost.setEntity(httpEntity);
-            try (final CloseableHttpResponse response = client.execute(httpPost)) {
-                HttpEntity responseEntity = response.getEntity();
-                if (logger.isLoggable(Level.INFO)) {
-                    logger.info("Event send successfully, and response: " + EntityUtils.toString(responseEntity));
+            new Thread(() -> myClient.sendPost(receiver, event, new FutureCallback<SimpleHttpResponse>() {
+                @Override
+                public void completed(SimpleHttpResponse response) {
+                    logger.info("Event send succeeded, response: " + response.getBodyText());
                 }
-                EntityUtils.consumeQuietly(responseEntity);
-            }
+
+                @Override
+                public void failed(Exception e) {
+                    logger.info("Event send has been failed, response: " + e);
+                }
+
+                @Override
+                public void cancelled() {
+                    logger.info("Event send has been cancelled");
+                }
+            })).start();
         } catch (Exception e) {
             logger.log(Level.WARNING, "Failed to send event to " + receiver, e);
         }
+
     }
 
+    static public class CustomCloseableHttpAsyncClient {
+        private final CloseableHttpAsyncClient httpClient;
+
+        {
+            PoolingAsyncClientConnectionManager cmb = PoolingAsyncClientConnectionManagerBuilder
+                    .create()
+                    .setMaxConnPerRoute(1)
+                    .setMaxConnTotal(1)
+                    .setConnectionTimeToLive(TimeValue.ofSeconds(10L))
+                    .build();
+
+            httpClient = HttpAsyncClients.custom().setConnectionManager(cmb).build();
+            httpClient.start();
+        }
+
+        public void sendPost(String receiver, Event event, final FutureCallback<SimpleHttpResponse> callback) {
+
+            String eventJSON = JSONObject.fromObject(event, new EventJsonConfig()).toString(4);
+
+            SimpleHttpRequest request = SimpleRequestBuilder
+                    .post(receiver)
+                    .setBody(eventJSON, ContentType.APPLICATION_JSON)
+                    .build();
+
+            httpClient.execute(request, callback);
+        }
+    }
 }
+
+
