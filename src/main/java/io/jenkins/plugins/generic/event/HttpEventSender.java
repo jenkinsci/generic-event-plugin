5123e60e83fe81d53ce794fe40da52939e638402
Nikita <nikita.kasatkin44@gmail.com>
2022-07-26 15:01:40 +0300

    Send Jenkins events asynchronously (#4)

--- src/main/java/io/jenkins/plugins/generic/event/HttpEventSender.java
+++ src/main/java/io/jenkins/plugins/generic/event/HttpEventSender.java
@@ -1,21 +1,22 @@
 package io.jenkins.plugins.generic.event;
 
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.apache.commons.lang.StringUtils;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.entity.EntityBuilder;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.ContentType;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.*;
 import io.jenkins.plugins.generic.event.json.EventJsonConfig;
 import io.jenkins.plugins.generic.event.transformer.EventDataTransformers;
 import net.sf.json.JSONObject;
+import org.apache.hc.core5.util.TimeValue;
 
 /**
  * An implementation of EventSender, which sends event body via HTTP.
@@ -25,36 +26,69 @@ import net.sf.json.JSONObject;
 public class HttpEventSender implements EventSender {
 
     private final Logger logger = Logger.getLogger(HttpEventSender.class.getName());
+    private final CustomCloseableHttpAsyncClient myClient = new CustomCloseableHttpAsyncClient();
 
     @Override
     public void send(Event event) {
         EventDataTransformers.INSTANCE.transform(event);
 
-        // serialize event to JSON string
-        String receiver = EventGlobalConfiguration.get().getReceiver();
+        final String receiver = EventGlobalConfiguration.get().getReceiver();
+
         if (StringUtils.isBlank(receiver)) {
             logger.info("Skipped event sending due to receiver URL not set");
             return;
         }
-        // TODO Make HTTP request asynchronous
-        try (final CloseableHttpClient client = HttpClients.createDefault()) {
-            String eventJSON = JSONObject.fromObject(event, new EventJsonConfig()).toString(4);
-            HttpEntity httpEntity = EntityBuilder.create()
-                    .setContentType(ContentType.APPLICATION_JSON)
-                    .setText(eventJSON)
-                    .build();
-            HttpPost httpPost = new HttpPost(receiver);
-            httpPost.setEntity(httpEntity);
-            try (final CloseableHttpResponse response = client.execute(httpPost)) {
-                HttpEntity responseEntity = response.getEntity();
-                if (logger.isLoggable(Level.INFO)) {
-                    logger.info("Event send successfully, and response: " + EntityUtils.toString(responseEntity));
+
+        try {
+            myClient.sendPost(receiver, event, new FutureCallback<SimpleHttpResponse>() {
+                @Override
+                public void completed(SimpleHttpResponse response) {
+                    logger.info("Event send succeeded, response: " + response.getBodyText());
                 }
-                EntityUtils.consumeQuietly(responseEntity);
-            }
+
+                @Override
+                public void failed(Exception e) {
+                    logger.info("Event send has been failed, response: " + e);
+                }
+
+                @Override
+                public void cancelled() {
+                    logger.info("Event send has been cancelled");
+                }
+            });
         } catch (Exception e) {
             logger.log(Level.WARNING, "Failed to send event to " + receiver, e);
         }
+
     }
 
+    static public class CustomCloseableHttpAsyncClient {
+
+        private static final CloseableHttpAsyncClient httpClient;
+
+        static {
+            PoolingAsyncClientConnectionManager cmb = PoolingAsyncClientConnectionManagerBuilder
+                    .create()
+                    .setMaxConnPerRoute(1)
+                    .setMaxConnTotal(1)
+                    .setConnectionTimeToLive(TimeValue.ofSeconds(10L))
+                    .build();
+
+            httpClient = HttpAsyncClients.custom().setConnectionManager(cmb).build();
+            httpClient.start();
+        }
+        public void sendPost(String receiver, Event event, final FutureCallback<SimpleHttpResponse> callback) {
+
+            String eventJSON = JSONObject.fromObject(event, new EventJsonConfig()).toString(4);
+
+            SimpleHttpRequest request = SimpleRequestBuilder
+                    .post(receiver)
+                    .setBody(eventJSON, ContentType.APPLICATION_JSON)
+                    .build();
+
+            httpClient.execute(request, callback);
+        }
+    }
 }
+
+
